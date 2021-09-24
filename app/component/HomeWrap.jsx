9713a69dd7cf9e3ed3f9864ee0da4bc9e56749d8
孙中锋 <465066260@qq.com>
2021-09-24 11:36:08 +0800

    快捷菜单

--- app/component/HomeWrap.jsx
+++ app/component/HomeWrap.jsx
@@ -4,11 +4,17 @@ import '../assets/css/home-wrap.scss';
 import { Utils } from "../common";
 import NavLink from "../common/NavLink";
 
+import classnames from 'classnames';
+
+const analysisTypes = [{ key: 1, label: '性格测试' }, { key: 2, label: '职场测试' }, { key: 3, label: '亲子测试' }, { key: 4, label: '情感测试' }];
+
 export default class HomeWrap extends React.Component {
 
     constructor(props) {
         super(props);
-        this.state = {};
+        this.state = {
+            showPiker: false
+        };
     }
 
     componentDidMount() {
@@ -19,7 +25,13 @@ export default class HomeWrap extends React.Component {
         });
     }
 
+    showPicker = (showPiker = false) => {
+        this.setState({ showPiker })
+    }
+
     render() {
+
+        let { showPiker } = this.state;
 
         return <div className='home-wrap'>
 
@@ -30,16 +42,34 @@ export default class HomeWrap extends React.Component {
                         <p>首页</p>
                     </NavLink>
                 </li>
-                <li className="c-btn">
+                <li className="c-btn" onClick={() => {
+                    this.showPicker(true);
+                }}>
                     <div className="icon start" />
                 </li>
                 <li>
                     <NavLink to='/profile'>
                         <div className="icon profile" />
-                        <p>个人中心</p>
+                        <p>我的</p>
                     </NavLink>
                 </li>
             </ul>
+
+            <div className={classnames("overlay", { 'overlay-fadein': showPiker }, { 'overlay-fadeout': !showPiker })} onClick={() => this.showPicker()} />
+            <div className={classnames('start-picker', { 'start-picker-fadein': showPiker }, { 'start-picker-fadeout': !showPiker })}>
+                <ul>
+                    {analysisTypes.map((t, i) => {
+                        let { key, label } = t;
+                        return <li key={i}>
+                            <i className={`i-${key}`} />
+                            <p>{label}</p>
+                        </li>
+
+                    })}
+                </ul>
+
+                <div className="close" onClick={() => this.showPicker()} />
+            </div>
 
         </div>;
     }
